b6fd41cc311c9c246ae0516779c88e96f95cbaf6
Matvii Mykhailichenko <103883982+matthewthefool@users.noreply.github.com>
2023-12-11 21:22:42 +0100

    Update SRP042620.R

--- Scripts/SRP042620.R
+++ Scripts/SRP042620.R
@@ -19,9 +19,41 @@ proj_info <- subset(
 )
 SRP042620 <- create_rse(proj_info)
 
-colData(SRP042620) %>%
-  as.data.frame() %>%
-  .$sra.sample_attributes
+SRP042620_expanded_sample_attributes <- expand_sra_attributes(SRP042620)
+
+# Checking if dataset has miRNA, lncRNA and mRNA
+rowData(SRP042620_expanded_sample_attributes) %>%
+  .$gene_type %>%
+  unique()
+
+# Computing read counts from base-pair counts
+read_counts <- compute_read_counts(SRP042620_expanded_sample_attributes)
+
+colData(SRP042620_expanded_sample_attributes) %>%
+  .$sra_attribute.source_name %>%
+  unique()
+
+# Create a function to determine the condition
+determine_condition <- function(source_name) {
+  if (source_name %in% c("ER+ Breast Cancer Primary Tumor", "Breast Cancer Cell Line", 
+                         "Triple Negative Breast Cancer Primary Tumor")) {
+    return("cancer")
+  } else if (source_name %in% c("Uninvolved Breast Tissue Adjacent to ER+ Primary Tumor", 
+                                "Reduction Mammoplasty - No known cancer", 
+                                "Uninvolved Breast Tissue Adjacent to TNBC Primary Tumor")) {
+    return("normal")
+  } else {
+    return(NA)  # or some default value
+  }
+}
+
+# Apply the function to create the new column
+colData(SRP042620_expanded_sample_attributes)$condition <- sapply(colData(SRP042620_expanded_sample_attributes)$`sra_attribute.source_name`, determine_condition)
+
+# Making DeSeqDataSet object from matrix
+dds <- DESeqDataSetFromMatrix(countData = read_counts,
+                              colData = colData(SRP042620_expanded_sample_attributes),
+                              design = ~ condition)
 
 
 
