83611ca6b0972ebfd2c8d05f7f1366ca3bea97c7
Matvii Mykhailichenko <103883982+matthewthefool@users.noreply.github.com>
2023-12-26 21:54:21 +0100

    Update Involved_vs_uninvolved.R

--- Scripts/Involved_vs_uninvolved.R
+++ Scripts/Involved_vs_uninvolved.R
@@ -1,28 +1,32 @@
+library(ComplexHeatmap)
+library(RColorBrewer)
+library(circlize)
+
 # Create a vector of tissue types you want to keep
 tissue_types_to_keep <- c("ER+ Breast Cancer Primary Tumor", "Uninvolved Breast Tissue Adjacent to ER+ Primary Tumor")
 
 # Subset the cells based on the tissue types
-ER_pos_rse <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
+rse_ER_pos <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
 
-ER_pos_rse %>%
+rse_ER_pos %>%
   colData() %>%
   .$sra_attribute.tissue
 
-ER_pos_colData <- colData(subset_rse) %>% 
+colData_ER_pos <- colData(subset_rse) %>% 
   as.data.frame() %>%
   .$condition %>%
   data.frame(condition=.)
 
-ER_pos_dds <- DESeqDataSetFromMatrix(countData = assay(ER_pos_rse),
-                                     colData = ER_pos_colData,
+dds_ER_pos <- DESeqDataSetFromMatrix(countData = assay(rse_ER_pos),
+                                     colData = colData_ER_pos,
                                      design = ~ condition)
 
 #Performing variance stabilizing transformation (vst)
-ER_pos_vsd <- vst(ER_pos_dds)
+vsd_ER_pos <- vst(dds_ER_pos)
 
 # Perform PCA
-PCA_plot <- plotPCA(ER_pos_vsd, intgroup = "condition")
-PCA_plot
+PCA_plot_ER_pos <- plotPCA(vsd_ER_pos, intgroup = "condition")
+PCA_plot_ER_pos
 
 SRP042620_expanded_sample_attributes %>%
   colData() %>%
@@ -32,27 +36,27 @@ SRP042620_expanded_sample_attributes %>%
 tissue_types_to_keep <- c("Uninvolved Breast Tissue Adjacent to TNBC Primary Tumor", "Triple Negative Breast Cancer Primary Tumor")
 
 # Subset the cells based on the tissue types
-TBNC_rse <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
+rse_TBNC <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
 
-TBNC_rse %>%
+rse_TBNC %>%
   colData() %>%
   .$sra_attribute.tissue
 
-TBNC_colData <- colData(TBNC_rse) %>% 
+colData_TBNC <- colData(rse_TBNC) %>% 
   as.data.frame() %>%
   .$condition %>%
   data.frame(condition=.)
 
-TBNC_dds <- DESeqDataSetFromMatrix(countData = assay(TBNC_rse),
-                                     colData = TBNC_colData,
+dds_TBNC <- DESeqDataSetFromMatrix(countData = assay(rse_TBNC),
+                                     colData = colData_TBNC,
                                      design = ~ condition)
 
 #Performing variance stabilizing transformation (vst)
-TBNC_vsd <- vst(TBNC_dds)
+vsd_TBNC <- vst(dds_TBNC)
 
 # Perform PCA
-PCA_plot <- plotPCA(TBNC_vsd, intgroup = "condition")
-PCA_plot
+PCA_plot_TBNC <- plotPCA(vsd_TBNC, intgroup = "condition")
+PCA_plot_TBNC
 
 SRP042620_expanded_sample_attributes %>%
   colData() %>%
@@ -62,44 +66,89 @@ SRP042620_expanded_sample_attributes %>%
 tissue_types_to_keep <- c("Breast Cancer Cell Line", "Reduction Mammoplasty - No known cancer")
 
 # Subset the cells based on the tissue types
-BC_cell_line_rse <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
+rse_BC_cell_line <- SRP042620_expanded_sample_attributes[, colData(SRP042620_expanded_sample_attributes)$sra_attribute.tissue %in% tissue_types_to_keep]
 
-BC_cell_line_rse %>%
+rse_BC_cell_line %>%
   colData() %>%
   .$sra_attribute.tissue
 
-BC_cell_line_colData <- colData(BC_cell_line_rse) %>% 
+colData_BC_cell_line <- colData(rse_BC_cell_line) %>% 
   as.data.frame() %>%
   .$condition %>%
   data.frame(condition=.)
 
-BC_cell_line_dds <- DESeqDataSetFromMatrix(countData = assay(BC_cell_line_rse),
-                                   colData = BC_cell_line_colData,
+dds_BC_cell_line <- DESeqDataSetFromMatrix(countData = assay(rse_BC_cell_line),
+                                   colData = colData_BC_cell_line,
                                    design = ~ condition)
 
 #Performing variance stabilizing transformation (vst)
-BC_cell_line_vsd <- vst(BC_cell_line_dds)
+vsd_BC_cell_line <- vst(dds_BC_cell_line)
 
 # Perform PCA
-PCA_plot <- plotPCA(BC_cell_line_vsd, intgroup = "condition")
-PCA_plot
+PCA_plot_BC_cell_line <- plotPCA(vsd_BC_cell_line, intgroup = "condition")
+PCA_plot_BC_cell_line
 
-ER_pos_dds <- DESeq(ER_pos_dds)
+# ER_pos DEGs Analysis
 
-ER_pos_results <- results(ER_pos_dds, contrast = c('condition','cancer','normal'))
+dds_ER_pos <- DESeq(dds_ER_pos)
 
-ER_pos_DEGs <- subset(ER_pos_results, padj < 0.05 & abs(log2FoldChange) > 2 & baseMean > 1000 )
+results_ER_pos <- results(dds_ER_pos, contrast = c('condition','cancer','normal'))
 
-ER_pos_DEGs <- ER_pos_DEGs[order(ER_pos_DEGs$log2FoldChange, decreasing = TRUE),]
+DEGs_ER_pos <- subset(results_ER_pos, padj < 0.05 & abs(log2FoldChange) > 2 & baseMean > 1000 )
 
-DEGs_subset_ER_pos <- ER_pos_dds[rownames(ER_pos_dds) %in% rownames(ER_pos_DEGs),]
+DEGs_ER_pos <- DEGs_ER_pos[order(DEGs_ER_pos$log2FoldChange, decreasing = TRUE),]
 
-mat<-assay(DEGs_subset_ER_pos)[rownames(DEGs_subset_ER_pos), rownames(colData(DEGs_subset_ER_pos))] #sig genes x samples
+DEGs_dds_ER_pos <- dds_ER_pos[rownames(dds_ER_pos) %in% rownames(DEGs_ER_pos),]
 
-base_mean <- rowMeans(mat)
-mat.scaled <- t(apply(mat, 1, scale)) #center and scale each column (Z-score) then transpose
-colnames(mat.scaled) <- colnames(mat)
+DEGs_counts_ER_pos<-assay(DEGs_dds_ER_pos)[rownames(DEGs_dds_ER_pos), rownames(colData(DEGs_dds_ER_pos))] #sig genes x samples
 
+
+# Function to generate heatmap, taking a DEGs_counts matrix and a dds object
+generate_heatmap <- function(DEGs_counts, dds) {
+  DEGs_scaled_counts<- t(apply(DEGs_counts, 1, scale)) #center and scale each column (Z-score) then transpose
+  colnames(DEGs_scaled_counts) <- colnames(DEGs_counts)
+  num_keep <- 25
+  rows_keep <- c(seq(1:num_keep), seq((nrow(DEGs_scaled_counts) - num_keep + 1), nrow(DEGs_scaled_counts)))
+  
+  condition_vector <- dds %>%
+    colData() %>%
+    .$condition
+  
+  h1 <- HeatmapAnnotation(
+    Condition = condition_vector,  # Use the 'condition' column as annotation data
+    col = list(Condition = c("cancer" = "red", "normal" = "blue"))  # Define colors for annotation levels
+  ) %>%
+    Heatmap(
+      DEGs_scaled_counts[rows_keep, ],
+      cluster_rows = FALSE,
+      column_labels = colnames(DEGs_scaled_counts),
+      name = "Z-score",
+      cluster_columns = TRUE,
+      top_annotation = .,
+      show_column_names = FALSE
+    )
+  
+  return(h1)
+}
+
+Heatmap_ER_pos <- generate_heatmap(DEGs_counts_ER_pos, ER_pos_dds)
+Heatmap_ER_pos
+
+# TBNC DEGs Analysis
+
+dds_TBNC <- DESeq(dds_TBNC)
+results_TBNC <- results(dds_TBNC, contrast = c('condition','cancer','normal'))
+DEGs_TBNC <- subset(results_TBNC, padj < 0.05 & abs(log2FoldChange) > 2 & baseMean > 1000 )
+DEGs_TBNC <- DEGs_TBNC[order(DEGs_TBNC$log2FoldChange, decreasing = TRUE),]
+DEGs_dds_TBNC <- dds_TBNC[rownames(dds_TBNC) %in% rownames(DEGs_TBNC),]
+DEGs_counts_TBNC <-assay(DEGs_dds_TBNC)[rownames(DEGs_dds_TBNC), rownames(colData(DEGs_dds_TBNC))] #sig genes x samples
+
+Heatmap_TBNC <- generate_heatmap(DEGs_counts_TBNC, dds_TBNC)
+Heatmap_TBNC
+
+
+
+# Code to map ENSEMBL ids
 ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
 
 gene_info <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), 
@@ -108,57 +157,3 @@ gene_info <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
                    mart = ensembl)
 gene_info
 
-listAttributes(ensembl)
-
-
-
-rownames(mat.scaled) <- gene_info$external_gene_name
-library(biomaRt)
-
-num_keep <- 25
-
-rows_keep <- c(seq(1:num_keep), seq((nrow(mat.scaled)-num_keep), nrow(mat.scaled)) )
-
-library(ComplexHeatmap)
-library(RColorBrewer)
-library(circlize)
-
-
-heatmap_annotation <- HeatmapAnnotation(
-  Condition = column_colors$condition,  # Use the 'condition' column as annotation data
-  col = list(Condition = c("cancer" = "red", "normal" = "blue"))  # Define colors for annotation levels
-)
-
-h1 <- Heatmap(mat.scaled[rows_keep,], cluster_rows = F, 
-              column_labels = colnames(mat.scaled), name="Z-score",
-              cluster_columns = T,
-              top_annotation = heatmap_annotation,
-              show_column_names = FALSE)
-
-h1
-
-column_colors <- ER_pos_dds %>%
-  colData() %>%
-  .$condition %>%
-  data.frame(condition=.)
-
-rownames(column_colors) <- ER_pos_dds %>%
-  colnames()
-
-# Create a color mapping for the conditions
-color_mapping <- c("cancer" = "1", "normal" = "-1")
-
-# Assuming your dataframe is called 'df'
-# Add a new column 'color' based on the 'condition' column
-column_colors <- column_colors %>%
-  mutate(color = color_mapping[condition])
-
-
-
-col_fun = colorRamp2(c(-1, 1), c("blue", "red"))
-
-
-column_colors <- column_colors[, 2, drop = TRUE] %>%
-  as.numeric()
-
-column_colors <- col_fun(column_colors)
