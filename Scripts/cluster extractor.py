06e9ec321fda2f192b0321bc1e6126d2679bfda7
redegator <redegator@gmail.com>
2024-05-04 02:36:22 +0300

    Now it also saves unique genes from all the clusters to "DEGs_from_clusters.txt". Spoiler: all of them are unique

--- Scripts/cluster extractor.py
+++ Scripts/cluster extractor.py
@@ -8,6 +8,7 @@ with open('../Files/Clusters.txt', 'r') as file:
         if "Cluster	Score" in line:
             break
 
+    unique_ids = []
     # Iterate through each line that contains information about cluster
     for line in file:
         count +=1   # For file names
@@ -18,8 +19,14 @@ with open('../Files/Clusters.txt', 'r') as file:
 
         # Iterate through each part in the line to get IDs
         for part in parts:
+            # Add all genes to the list that will be saved to the file of cluster
             if '9606' in part:
-                ids.append(part.split('.')[1])
+                gene_to_add = part.split('.')[1]
+                ids.append(gene_to_add)
+
+                # Add all unique genes to the list that will be saved to the file with all genes
+                if gene_to_add not in unique_ids:
+                    unique_ids.append(gene_to_add)
 
         # Format the IDs before writing them to the output file
         ids_str = '\n'.join(ids)
@@ -28,3 +35,13 @@ with open('../Files/Clusters.txt', 'r') as file:
         with open(f"../Files/clusters/cluster{count}.txt", "w") as file_to_write:
             file_to_write.write(ids_str)
 
+
+# Remove '\n' characters after gene name at the end of the cluster
+unique_ids_without_newline = [id.rstrip('\n') for id in unique_ids]
+
+# Format the IDs before writing them to the output file
+unique_ids_without_newline_str = '\n'.join(unique_ids_without_newline)
+
+# Write all the IDs to one file
+with open(f"../Files/DEGs_from_clusters.txt", "w") as file_to_write:
+    file_to_write.write(unique_ids_without_newline_str)
